1d3117779696e49064b3382c7dd089e3a871f67e
SojihAhmed114 <sojib.ahmed7620@gmail.com>
2024-01-26 15:47:20 +0600

    operation done

--- operation.js
+++ operation.js
@@ -1,4 +1,4 @@
-// addition 
+
 var mama = 50;
 var mami = 800;
 var mot = mama + mami;
@@ -15,3 +15,29 @@ var subtotal = chotobon - borobon;
 console.log(subtotal);
 
 
+var tk = 500;
+var tk2 = 21;
+var submot = tk / tk2;
+console.log(submot.toFixed(2));
+
+
+var tk6 = 50;
+var tk8 = 23;
+var kichu = tk6 * tk8;
+console.log(kichu);
+
+var tkThree = 500;
+var tkFour = 21;
+var Vagsesh = tkThree % tkFour;
+console.log(Vagsesh);
+
+
+// values change
+
+var orange = 50;
+orange = orange + 10;
+
+var price = orange * 2;
+var discountPrice = price - 20;
+var newPrice = discountPrice + 50;
+console.log(newPrice);
