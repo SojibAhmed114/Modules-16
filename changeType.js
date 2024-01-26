3121f5499eb97c3c64d88983804301c20c08059a
SojihAhmed114 <sojib.ahmed7620@gmail.com>
2024-01-26 13:49:09 +0600

    string to number convert use parseInt

---
+++ changeType.js
@@ -0,0 +1,14 @@
+var applePrice = "540";    //value is a number but it is a string type variable
+
+
+var price = "5481";
+console.log(typeof price);
+
+var pro = parseInt("5481");
+console.log(typeof pro);
+
+//convert string to number types variables
+
+var balance = parseInt("2541564");
+console.log(typeof balance);
+
