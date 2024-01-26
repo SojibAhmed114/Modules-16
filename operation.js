1ebfcbaed1b2aa42544269914f099003f1878dac
SojihAhmed114 <sojib.ahmed7620@gmail.com>
2024-01-26 15:21:13 +0600

    javascript ar operation kortechi

---
+++ operation.js
@@ -0,0 +1,17 @@
+// addition 
+var mama = 50;
+var mami = 800;
+var mot = mama + mami;
+console.log(mot)
+
+var userInput = "4581";
+var usrPass = 19;
+userInput = parseInt("4581");
+console.log(userInput + usrPass);
+
+var chotobon = 5200;
+var borobon = 200;
+var subtotal = chotobon - borobon;
+console.log(subtotal);
+
+
