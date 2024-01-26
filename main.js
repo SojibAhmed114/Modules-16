40cd4cf6d6d91d98f512c3f1d922e2ab0ab5ca38
SojihAhmed114 <sojib.ahmed7620@gmail.com>
2024-01-26 11:20:05 +0600

    javascript run

---
+++ main.js
@@ -0,0 +1,26 @@
+var sojib = (54);
+sojib = (41);
+console.log(sojib)
+
+var price = (250);
+console.log(price);
+
+ var x = 51
+var y = 68
+var z = x + y;
+console.log(z);
+
+var styType = "sojib ahmed safi"
+console.log(styType);
+
+var styType = "sojib ahmed safi"
+console.log(typeof styType);
+
+var checkType = "52";
+console.log(typeof checkType);
+
+// var checkTwo = sojib;
+// console.log(checkTwo);       //error
+
+var boolType = true;
+console.log(typeof boolType);
---
+++ types.js
@@ -0,0 +1,13 @@
+// number type
+
+var numType = 2541;
+console.log(numType);
+
+// boolean type
+var boolType = false;
+console.log(boolType);
+console.log(typeof boolType);
+
+// string type
+var strType = "I live in Bangaldesh"
+console.log(strType);
