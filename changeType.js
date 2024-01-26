542473a57cc9a26e039f708bcf5fbffc52f90fe1
SojihAhmed114 <sojib.ahmed7620@gmail.com>
2024-01-26 14:46:43 +0600

    .toFixed added

--- changeType.js
+++ changeType.js
@@ -2,13 +2,43 @@ var applePrice = "540";    //value is a number but it is a string type variable
 
 
 var price = "5481";
-console.log(typeof price);
+// console.log(typeof price);
 
 var pro = parseInt("5481");
-console.log(typeof pro);
+// console.log(typeof pro);
 
 //convert string to number types variables
 
 var balance = parseInt("2541564");
-console.log(typeof balance);
+// console.log(typeof balance);
 
+var money = parseInt("521.23");
+// console.log(money);
+
+var tk = parseFloat("523.12");
+// console.log(tk);
+
+var orange = 25.25;
+var orange2 = 52.32;
+var total = orange + orange2;
+// console.log(total);
+
+var banana = "52.33";
+var banana2 = 45.25;
+banana = parseFloat(banana);
+var mot = banana + banana2;
+console.log(mot);
+
+
+// number type to string type
+var myMark = 25;
+myMark = "" + myMark;
+console.log(typeof myMark);
+
+
+// tofixed use
+
+var karun = 0.1;
+var shohan = 0.2;
+var duijon = karun + shohan;
+console.log(duijon.toFixed(5));
