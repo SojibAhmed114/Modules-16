e32b35e215b88a9c7986be0c49184886891dca9c
SojihAhmed114 <sojib.ahmed7620@gmail.com>
2024-01-26 20:36:07 +0600

    ok bye

---
+++ extra.js
@@ -0,0 +1,9 @@
+//  number type to string type convert
+var price = 410;
+price = "" + price;
+console.log(typeof price);
+
+// string type to number type convert
+var a = "560";
+a = parseInt(a);
+console.log(typeof a);
